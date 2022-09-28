c968e6722ce4f54160d243bf63bb882cec02ebe6
Wayf1nder <114050923+Wayf1nder@users.noreply.github.com>
2022-09-28 08:25:15 +0300

    update
    no repeat

--- rps_game.py
+++ rps_game.py
@@ -1,13 +1,24 @@
-human_turn = input('input human turn: ')
-computer_turn = input('input computer turn: ')
+#Uzdevums 1 : modificēt kodu lai atkārtojas (Done)
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('human wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('human wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('human wins!')
-else:
-    print('computer wins!')
+import random
+turns = ['rock', 'paper', 'scissors']
+
+
+while (True):
+    human_turn = input('Enter your turn, or exit!:')
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        break
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('human wins!')
+    else:
+        print('computer wins!')
